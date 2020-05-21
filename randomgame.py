30555da9196ec9e7192b85768f72bf0521fb2853
Geert <geertvk89@hotmail.com>
2020-05-21 14:39:39 +0200

    Added randomgame.py

---
+++ randomgame.py
@@ -0,0 +1,23 @@
+from random import randint
+import sys
+# you will need to run this on your machine and not this website for the sys.argv to work!
+
+
+answer = randint(sys.argv[1], sys.argv[2])
+
+while True:
+    try:
+        print(answer)
+        guess = int(input('Guess a number between 1 ~ 10:  '))
+
+        if 0 < guess < 11:
+            if guess == answer:
+                print('You are a genius')
+                break
+        else:
+            print('Hey smartass, I said 1~10')
+    except ValueError:
+        print('Please enter a number')
+        continue
+
+
